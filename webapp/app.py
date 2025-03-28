4eb019d66dcfd41d7e8b3fca124e4d17e7d3b000
adriano22_ <califano993@gmail.com>
2025-03-28 14:39:24 +0100

    Added crawling parameters form to web-app

--- webapp/app.py
+++ webapp/app.py
@@ -7,6 +7,19 @@ def index():
     return flask.render_template('index.html')
 
 
+# TODO: change this to an azure functions-call, and verify the correctness of the azure-function step for the crawling setup.
+@app.route('/setup-crawl', methods = ["GET", "POST"])
+def setup_crawl():
+    crawler_depth = flask.request.form["crawler-depth"]
+    max_links = flask.request.form["max-links"]
+    max_exec_time = flask.request.form["max-execution-time"]
+    concurrent_workers = flask.request.form["concurrent-workers"]
+    start_url = flask.request.form["start-url"]
+    
+    text = f"{crawler_depth}, {max_links}, {max_exec_time}, {concurrent_workers}, {start_url}"
+    response = flask.make_response(text, 200)
+    response.mimetype = "text/plain"
+    return response
 
 if __name__ == "__main__":
     app.run(port = 8080, debug = True)
--- webapp/static_files/main.js
+++ webapp/static_files/main.js
@@ -1,3 +1,6 @@
-$(document).ready(function() {
-    
+$(document).ready(function(){
+    $("#continue-button").click(function(){
+        var startUrlValue = document.getElementById("start-url").value;
+        document.getElementById("hidden-start-url").value = startUrlValue;
+    });
 });
