73e5d444c061548551a46ba00937a95ff386edfb
adriano22_ <califano993@gmail.com>
2025-03-28 11:34:59 +0100

    test push with github action

--- functions/function-app/function_app.py
+++ functions/function-app/function_app.py
@@ -20,6 +20,6 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "Updated function has been executed successfully!",
              status_code=200
         )
